fe0437f422986383bb1bcecc9a7a4237d30746e3
Alamin-22 <mdalaminmollick@gmail.com>
2025-01-29 23:57:47 +0600

    created folder structre and main router to connect all sub routers

---
+++ src/app/routers/index.ts
@@ -0,0 +1,14 @@
+import { Router } from "express";
+
+const router = Router();
+
+// const moduleRouters = [
+// //   {
+// //     path: "/admins",
+// //     route: AdminRoutes,
+// //   },
+// ];
+
+// moduleRouters.forEach((router) => router.use(router.push, router.route));
+
+// export default router;
