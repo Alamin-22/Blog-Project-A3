a4dce0dda12ac1917b155cf39349a29ed5cb8725
Alamin-22 <mdalaminmollick@gmail.com>
2025-02-06 09:58:42 +0600

    working on to create get all user logic

--- src/app/modules/User/user.service.ts
+++ src/app/modules/User/user.service.ts
@@ -1,1 +1,27 @@
-export const userServices = {};
+import QueryBuilder from '../../builder/QueryBuilder';
+import { userSearchableField } from './user.constant';
+import { UserModel } from './user.model';
+
+const getAllUserFromDB = async (query: Record<string, unknown>) => {
+  const userQuery = new QueryBuilder(
+    UserModel.find()
+      .populate('admissionSemester')
+      .populate({
+        path: 'academicDepartment',
+        populate: {
+          path: 'academicFaculty',
+        },
+      }),
+    query,
+  )
+    .search(userSearchableField)
+    .filter()
+    .sort()
+    .paginate()
+    .fields();
+
+  const result = await userQuery.modelQuery;
+  return result;
+};
+
+export const userServices = { getAllUserFromDB };
