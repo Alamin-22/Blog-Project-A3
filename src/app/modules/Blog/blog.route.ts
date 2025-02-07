aadd2590f3ec65dbe078f678aebf274a717953ae
Alamin-22 <mdalaminmollick@gmail.com>
2025-02-07 19:55:40 +0600

    modified the logic of validaiton

--- src/app/modules/Blog/blog.route.ts
+++ src/app/modules/Blog/blog.route.ts
@@ -1,9 +1,15 @@
 import express from 'express';
 import { BlogControllers } from './blog.controller';
+import ValidateRequestMiddleWare from '../../middlewares/validateRequest';
+import { BlogValidations } from './blog.validation';
 
 const router = express.Router();
 
-router.post('/', BlogControllers.createBlog);
+router.post(
+  '/',
+  ValidateRequestMiddleWare(BlogValidations.createBlogValidationSchema),
+  BlogControllers.createBlog,
+);
 
 // router.get('/:id', userControllers.getSingleUser);
 
